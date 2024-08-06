ff3e542be514406f9f83e9b96f26815ec605cc1f
StarkBak <sstechnologyindtec@gmail.com>
2024-08-06 20:48:53 +0430

    DTOs  added

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,19 @@
-export class CreateAuthDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAuthDto {
+  @ApiProperty({ example: 'john-doe' })
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @ApiProperty({ example: 'john.doe@example.com' })
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({ example: 'password123' })
+  password: string;
+}
